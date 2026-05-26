9568f22323abcae334704c5351b5a43cede7c075
Snehanand Yadav <snehanandyadav18@gmail.com>
2026-05-26 11:19:35 +0530

    removed repository for deploy

--- src/main/java/com/Snehanand/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Snehanand.repository;
-
-import com.Snehanand.model.Patient;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
--- src/main/java/com/Snehanand/service/PatientService.java
+++ src/main/java/com/Snehanand/service/PatientService.java
@@ -1,20 +1,14 @@
 package com.Snehanand.service;
 
 import com.Snehanand.model.Patient;
-import com.Snehanand.repository.PatientRepository;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
 
-    private final PatientRepository repository;
-
-    public PatientService(PatientRepository repository) {
-        this.repository = repository;
-    }
-
     public void addPatient(Patient patient) {
-        repository.save(patient);
+
+        System.out.println(patient.getName());
+
     }
 }
